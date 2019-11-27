c6f170fdd364b7a0f75c239e38b5bd291a8fcfcf
vapaavetehinen <sean.freemerman@gmail.com>
2019-11-27 18:26:12 -0500

    updated exampledata based on domain rename

--- src/ExampleData.js
+++ src/ExampleData.js
@@ -2,191 +2,191 @@ export const exampleData = [
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/268353.jpg",
     name: "LemonCloud Minecraft Server",
-    ip: "play.lemoncloud.net"
+    domain: "play.lemoncloud.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/448226.jpg",
     name: "MineSuperior Minecraft Server",
-    ip: "hub.mcs.gg"
+    domain: "hub.mcs.gg"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/289594.jpg",
     name: "Aspiria MC Minecraft Server",
-    ip: "mcsl.AspiriaMc.Com"
+    domain: "mcsl.AspiriaMc.Com"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/432219.jpg",
     name: "Detox Network Minecraft Server",
-    ip: "DETOX.GG"
+    domain: "DETOX.GG"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/250183.jpg",
     name: "PURPLE PRISON Minecraft Server",
-    ip: "PURPLE.WTF"
+    domain: "PURPLE.WTF"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/280197.jpg",
     name: "MineSpire Minecraft Server",
-    ip: "mc.minespire.net"
+    domain: "mc.minespire.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/437511.jpg",
     name: "InsanityCraft Minecraft Server",
-    ip: "play.insanitycraft.net"
+    domain: "play.insanitycraft.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/357211.jpg",
     name: "Cultivate Minecraft Server",
-    ip: "play.cultivatemc.com"
+    domain: "play.cultivatemc.com"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/210879.jpg",
     name: "Peaceful Farms Network Minecraft Server",
-    ip: "play.peacefulfarms.net"
+    domain: "play.peacefulfarms.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/446946.jpg",
     name: "DuplexMC Minecraft Server",
-    ip: "play.duplexmc.net"
+    domain: "play.duplexmc.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/292028.jpg",
     name: "Hypixel Network Minecraft Server",
-    ip: "mc.hypixel.net"
+    domain: "mc.hypixel.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/440631.jpg",
     name: "VanityMC Minecraft Server",
-    ip: "pvp.vanitymc.co"
+    domain: "pvp.vanitymc.co"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/324937.jpg",
     name: "ExtremeCraft Minecraft Server",
-    ip: "play.extremecraft.net"
+    domain: "play.extremecraft.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/322140.jpg",
     name: "Minecraft Central Minecraft Server",
-    ip: "mccentral.org"
+    domain: "mccentral.org"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/411920.jpg",
     name: "MC-Complex Minecraft Server",
-    ip: "pixel.mc-complex.com"
+    domain: "pixel.mc-complex.com"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/392682.jpg",
     name: "PvPWars Minecraft Server",
-    ip: "play.pvpwars.net"
+    domain: "play.pvpwars.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/284534.jpg",
     name: "mineheroes.net Minecraft Server",
-    ip: "mineheroes.net"
+    domain: "mineheroes.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/279300.jpg",
     name: "ManaCube Minecraft Server",
-    ip: "play.manacube.net"
+    domain: "play.manacube.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/330484.jpg",
     name: "APPLECRAFT! Minecraft Server",
-    ip: "play.applecraft.org"
+    domain: "play.applecraft.org"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/426783.jpg",
     name: "Performium Network Minecraft Server",
-    ip: "MC.Performium.net"
+    domain: "MC.Performium.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/73294.jpg",
     name: "VortexPvP Minecraft Server",
-    ip: "mcsl.vortexpvp.gg"
+    domain: "mcsl.vortexpvp.gg"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/247575.jpg",
     name: "DestinyMC Minecraft Server",
-    ip: "play.thedestinymc.com"
+    domain: "play.thedestinymc.com"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/433428.jpg",
     name: "SkyBlock Network Minecraft Server",
-    ip: "play.skyblocknetwork.com"
+    domain: "play.skyblocknetwork.com"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/198426.jpg",
     name: "FadeCloud Minecraft Server",
-    ip: "FadeCloud.com"
+    domain: "FadeCloud.com"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/210232.jpg",
     name: "GrandTheftMinecart Minecraft Server",
-    ip: "mc-gtm.net"
+    domain: "mc-gtm.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/326046.jpg",
     name: "SkyCade Minecraft Server",
-    ip: "play.skycade.net"
+    domain: "play.skycade.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/444110.jpg",
     name: "Vulengate Minecraft Server",
-    ip: "play.vulengate.com"
+    domain: "play.vulengate.com"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/231780.jpg",
     name: "MomentoNetwork Minecraft Server",
-    ip: "mc.momentonetwork.net"
+    domain: "mc.momentonetwork.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/85908.jpg",
     name: "Snapcraft Minecraft Server",
-    ip: "mc.snapcraft.net"
+    domain: "mc.snapcraft.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/260405.jpg",
     name: "Skyblock Minecraft Server",
-    ip: "us.skyblock.net"
+    domain: "us.skyblock.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/208964.jpg",
     name: "Desteria Minecraft Server",
-    ip: "pvp.desteria.net"
+    domain: "pvp.desteria.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/443757.jpg",
     name: "StrongCraft.org Minecraft Server",
-    ip: "play.strongcraft.org"
+    domain: "play.strongcraft.org"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/440455.jpg",
     name: "Minecraftchat Minecraft Server",
-    ip: "mc.minecraftchat.net"
+    domain: "mc.minecraftchat.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/445266.jpg",
     name: "OPMines Minecraft Server",
-    ip: "opmines.net"
+    domain: "opmines.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/218820.jpg",
     name: "Mineplex Minecraft Server",
-    ip: "us.mineplex.com"
+    domain: "us.mineplex.com"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/442419.jpg",
     name: "DragonBlock Minecraft Server",
-    ip: "play.dragonblock.org"
+    domain: "play.dragonblock.org"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/285719.jpg",
     name: "VentureLand Minecraft Server",
-    ip: "mc.ventureland.net"
+    domain: "mc.ventureland.net"
   },
   {
     banner: "//cdn.minecraft-server-list.com/serverlogo/288369.jpg",
     name: "JartexNetwork Minecraft Server",
-    ip: "play.jartexnetwork.com"
+    domain: "play.jartexnetwork.com"
   }
 ];
