250e0282e5b4a7353ba65918e72273a2dfe5800f
vapaavetehinen <sean.freemerman@gmail.com>
2019-11-27 16:53:36 -0500

    no longer wrapping

--- src/App.svelte
+++ src/App.svelte
@@ -26,7 +26,7 @@
   {#each servers as { name, ip, banner }}
     <div class="server">
       <h2>{name}</h2>
-      <h2 class="ip">{ip}<button on:click={() => navigator.clipboard.writeText({ip}['ip'])}>Copy</button></h2>
+      <h2 class="ip">{ip}<button on:click={() => navigator.clipboard.writeText(ip)}>Copy</button></h2>
       <img src={banner} alt={`${name} banner`} />
     </div>
   {/each}
