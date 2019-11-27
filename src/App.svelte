56180260ca3b7d66ce69c6f312b7aa17aaedd036
vapaavetehinen <sean.freemerman@gmail.com>
2019-11-27 13:28:42 -0500

    added copy feature

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,14 @@
   import { exampleData } from "./ExampleData.js";
   export let servers = exampleData;
   console.log(exampleData);
+  export function grabip(text) {
+    let textbox = document.createElement('textarea');
+    textbox.value= text['ip'];
+    document.body.append(textbox);
+    textbox.select();
+    document.execCommand("copy");
+    textbox.remove();
+  } 
 </script>
 
 <style>
@@ -27,7 +35,7 @@
   {#each servers as { name, ip, banner }}
     <div class="server">
       <h2>{name}</h2>
-      <h2 class="ip">{ip}</h2>
+      <h2 class="ip">{ip}<button on:click={() => grabip({ip})}>Copy</button></h2>
       <img src={banner} alt={`${name} banner`} />
     </div>
   {/each}
