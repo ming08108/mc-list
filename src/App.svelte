d8fd23916f67b7a2024371413afcb497ccabc781
ming08108 <gyang15@illinois.edu>
2019-11-26 20:33:05 -0600

    init

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { exampleData } from "./ExampleData.js";
+  export let servers = exampleData;
+  console.log(exampleData);
+</script>
+
+<style>
+  main {
+    max-width: 80em;
+    margin: 0 auto;
+  }
+  .server {
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+  .ip {
+    float: right;
+  }
+</style>
+
+<head>
+  <meta name="Description" content="A minecraft server list." />
+</head>
+
+<main>
+  <h1>Minecraft Server List</h1>
+
+  {#each servers as { name, ip, banner }}
+    <div class="server">
+      <h2>{name}</h2>
+      <h2 class="ip">{ip}</h2>
+      <img src={banner} alt={`${name} banner`} />
+    </div>
+  {/each}
+</main>
