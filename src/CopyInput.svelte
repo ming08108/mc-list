8cf75920ab4a8d7ec98e6bcad596c3df54cf0b0c
ming <ming@mings-MBP.T-mobile.com>
2019-11-28 22:22:25 -0500

    add style

--- src/CopyInput.svelte
+++ src/CopyInput.svelte
@@ -1,12 +1,36 @@
 <script>
-  let inputfield;
+  import Icon from "fa-svelte";
+  import { faCopy } from "@fortawesome/free-solid-svg-icons/faCopy";
   export let ip;
 
-  let grabIp = function() {
+  const copyIcon = faCopy;
+  let inputfield;
+  const grabIp = function() {
     inputfield.select();
     document.execCommand("copy");
   };
 </script>
 
-<input bind:this={inputfield} value={ip} readonly />
-<button on:click={grabIp}>Copy</button>
+<style>
+  button {
+    border: none;
+    background-color: #008cba;
+    color: white;
+    padding: 0.5em;
+  }
+  input {
+    border: none;
+    width: 25em;
+  }
+  div {
+    display: flex;
+  }
+</style>
+
+<div>
+  <input bind:this={inputfield} value={ip} readonly />
+  <button on:click={grabIp}>
+    <Icon icon={copyIcon} />
+    Copy
+  </button>
+</div>
